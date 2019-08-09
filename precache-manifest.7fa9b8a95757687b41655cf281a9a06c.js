d49c47747a5513587bf3713f275c27c722309d56
Vital Medvedev <vitalmedvedev@Vitals-MacBook-Pro.local>
2019-08-09 17:07:54 +0300

    Updates

---
+++ precache-manifest.7fa9b8a95757687b41655cf281a9a06c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26a0e75b6ebe0572e94a69a1d8c1117",
+    "url": "/heatmap/index.html"
+  },
+  {
+    "revision": "066747f3b0587654d0ee",
+    "url": "/heatmap/static/css/2.509658c0.chunk.css"
+  },
+  {
+    "revision": "1c8f6181fd18bbba47f6",
+    "url": "/heatmap/static/css/main.836868e7.chunk.css"
+  },
+  {
+    "revision": "066747f3b0587654d0ee",
+    "url": "/heatmap/static/js/2.a49a4412.chunk.js"
+  },
+  {
+    "revision": "1c8f6181fd18bbba47f6",
+    "url": "/heatmap/static/js/main.944f0533.chunk.js"
+  },
+  {
+    "revision": "e6b2793c0c8da9575b6e",
+    "url": "/heatmap/static/js/runtime~main.6924498e.js"
+  },
+  {
+    "revision": "0651d2a2de4f1a880f224c8060fdacae",
+    "url": "/heatmap/static/media/bg-image.0651d2a2.png"
+  },
+  {
+    "revision": "02ac589f21a539962bf3bcd5ceeded24",
+    "url": "/heatmap/static/media/heat_bodyMale.02ac589f.svg"
+  },
+  {
+    "revision": "47d51d5264c7671d5750098667c252ce",
+    "url": "/heatmap/static/media/heat_headMale.47d51d52.svg"
+  },
+  {
+    "revision": "833935d5cdb7ac0f82cdcc3ac82d2d0d",
+    "url": "/heatmap/static/media/heat_legsMale.833935d5.svg"
+  },
+  {
+    "revision": "b9f7baea8c0a877e779776bef7bf0f03",
+    "url": "/heatmap/static/media/maleRed.b9f7baea.svg"
+  }
+]);
